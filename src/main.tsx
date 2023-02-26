f783827af24c6cba164d4eb406ce310b12225f60
loicbefve <befve.loic@gmail.com>
2023-02-26 13:42:26 +0100

    Add basic components and initialized the rooter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Header />
+    <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 )
