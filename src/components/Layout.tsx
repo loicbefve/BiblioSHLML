6af4960c7eb2ac8b80cf2a03fc910008750b52dd
loicbefve <befve.loic@gmail.com>
2023-02-27 01:38:04 +0100

    La page header me semble correcte

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,36 @@
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import styled from 'styled-components'
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  min-height: 100vh;
+
+  font-size: 12px;
+
+  @media (min-width: 576px) {
+    font-size: 14px;
+  }
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+  }
+
+  @media (min-width: 992px) {
+    font-size: 18px;
+  }
+`
 
 const Layout = () => {
   return (
-    <>
+    <LayoutWrapper>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </LayoutWrapper>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,72 @@
 import styled from 'styled-components'
 import shlml_logo from '../assets/shlml_logo.jpg'
+import { Col, Container, Row } from 'react-bootstrap'
 
-const HomeContainer = styled.div`
+const HomeContainer = styled(Container)`
   display: flex;
-  justify-content: center;
+`
+
+const StyledImg = styled.img`
+  width: 100%;
+`
+
+const TextCol = styled(Col)`
+  padding: 30px;
+  font-size: 120%;
+`
+
+const ImgCol = styled(Col)`
+  display: flex;
   align-items: center;
 `
 const Home = () => {
   return (
-    <HomeContainer>
-      <img src={shlml_logo} alt="Le logo de la SHLM" />
-      <div>
-        <p>
-          Notre Société possède un important fonds documentaire centré bien
-          évidemment sur la Lorraine.
-        </p>
-        <p>
-          Elle est sur demande à la disposition des chercheurs et érudits. Afin
-          de simplifier la recherche nous avons décomposé ces fonds en rubrique:
-        </p>
-        <ul>
-          <li>Les imprimés, plus de 17 000 ouvrages.</li>
-          <li>
-            Les factums, plus de 1 000 documents. Il n'est pas facile de trouver
-            un document, sans comprendre le contenu, aussi en demandant une
-            recherche sans critère, vous pouvez faire défiler les titres des
-            divers mémoires.
-          </li>
-          <li>
-            Fonds documentaires, plus de 1 400 ouvrages, ce sont surtout des
-            ouvrages de cours du XIXème siècle. Le défilement s'obtient par la
-            recherche sans critère.
-          </li>
-          <li>
-            Le fonds johannique, plus de 3 200 ouvrages, la Société a bénéficié
-            d'unimportant legs de livres sur Jeanne d'Arc.
-          </li>
-          <li>
-            Manuscrits, plus de 400, certains sont des originaux, d'autres des
-            copies. Attention, une partie de ce fonds est déposé aux archives
-            départementales et n'est pas consultable au Musée Lorrain. Le
-            défilement s'obtient par la recherche sans critère.
-          </li>
-          <li>
-            Index du Pays Lorrain, cette rubrique reprend la table d'index de
-            1904 à 2004 réalisée par Pierre Simonin. Ce qui permet une recherche
-            textuelle dans les rubriques.
-          </li>
-        </ul>
-      </div>
+    <HomeContainer as={Container}>
+      <Row>
+        <ImgCol md={6}>
+          <StyledImg src={shlml_logo} alt="Le logo de la SHLM" />
+        </ImgCol>
+        <TextCol md={6}>
+          <p>
+            Notre Société possède un important fonds documentaire centré bien
+            évidemment sur la Lorraine.
+          </p>
+          <p>
+            Elle est sur demande à la disposition des chercheurs et érudits.
+            Afin de simplifier la recherche nous avons décomposé ces fonds en
+            rubrique:
+          </p>
+          <ul>
+            <li>Les imprimés, plus de 17 000 ouvrages.</li>
+            <li>
+              Les factums, plus de 1 000 documents. Il n'est pas facile de
+              trouver un document, sans comprendre le contenu, aussi en
+              demandant une recherche sans critère, vous pouvez faire défiler
+              les titres des divers mémoires.
+            </li>
+            <li>
+              Fonds documentaires, plus de 1 400 ouvrages, ce sont surtout des
+              ouvrages de cours du XIXème siècle. Le défilement s'obtient par la
+              recherche sans critère.
+            </li>
+            <li>
+              Le fonds johannique, plus de 3 200 ouvrages, la Société a
+              bénéficié d'unimportant legs de livres sur Jeanne d'Arc.
+            </li>
+            <li>
+              Manuscrits, plus de 400, certains sont des originaux, d'autres des
+              copies. Attention, une partie de ce fonds est déposé aux archives
+              départementales et n'est pas consultable au Musée Lorrain. Le
+              défilement s'obtient par la recherche sans critère.
+            </li>
+            <li>
+              Index du Pays Lorrain, cette rubrique reprend la table d'index de
+              1904 à 2004 réalisée par Pierre Simonin. Ce qui permet une
+              recherche textuelle dans les rubriques.
+            </li>
+          </ul>
+        </TextCol>
+      </Row>
     </HomeContainer>
   )
 }
