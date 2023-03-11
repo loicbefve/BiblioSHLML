20ab43fa709398d359fd408c96eac8656a0ba5d8
loicbefve <befve.loic@gmail.com>
2023-03-11 21:41:51 +0100

    Définition de tous les types de résultats

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,4 +1,11 @@
-export interface ImprimeResearchResult {
+export interface ResearchResult {
+  metadatas: {
+    [key: string]: string;
+  };
+  urls: string[];
+}
+
+export interface ImprimeResearchResult extends ResearchResult {
   metadatas: {
     epi: string;
     travee: string;
@@ -13,6 +20,70 @@ export interface ImprimeResearchResult {
     commentaire: string;
   };
   urls: string[];
+}
+
+export interface FactumsResearchResult extends ResearchResult {
+  metadatas: {
+    cote: string;
+    tome: string;
+    type: string;
+    auteur: string;
+    titre: string;
+    couverture: string;
+    langue: string;
+    edition: string;
+    datation: string;
+    contenu: string;
+    etat: string;
+    note: string;
+    emplacement: string;
+  };
+  urls: string[];
+}
+
+export interface FondsDocumentaireResearchResult extends ResearchResult {
+  metadatas: {
+    carton: string;
+    fonds: string;
+    type_de_document: string;
+    auteur: string;
+    auteur_bis: string;
+    titre: string;
+    couverture: string;
+    langue: string;
+    edition: string;
+    datation: string;
+    contenu: string;
+    etat: string;
+    ancien_proprietaire: string;
+    notes: string;
+    don: string;
+    emplacement_initiale_dans_la_bibliotheque: string;
+  };
+  urls: string[];
+}
+
+export interface FondsJohanniquesResearchResult extends ResearchResult {
+  metadatas: {
+    epi: string;
+    travee: string;
+    tablette: string;
+    auteur: string;
+    titre: string;
+    annee: string;
+    cote: string;
+    etat: string;
+    metrage_ou_commentaire: string;
+    carton: string;
+  };
+  urls: string[];
+}
+
+export interface ManuscritsAndIndexResearchResult {
+  metadatas: {
+    id: string;
+    commentaires: string;
+  };
 }
 
 export enum PageState {
