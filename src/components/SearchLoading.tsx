e14e0ae2ad9f0c57edfe7699b79af7d31c8edd4d
loicbefve <befve.loic@gmail.com>
2023-03-11 22:50:39 +0100

    Use default ract-bootstrap spinner

--- src/components/SearchLoading.tsx
+++ src/components/SearchLoading.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Spinner } from 'react-bootstrap';
 
 const SearchLoadingContainer = styled.div`
   flex: 1;
@@ -10,37 +11,11 @@ const SearchLoadingContainer = styled.div`
   color: gray;
 `;
 
-const Spinner = styled.div`
-  border: 16px solid gray;
-  border-top: 16px dimgrey solid;
-  border-radius: 50%;
-  height: 120px;
-  width: 120px;
-  animation: spin 2s linear infinite;
-  /* 
-  border: 16px solid #f3f3f3; 
-  border-top: 16px solid #3498db;
-  border-radius: 50%;
-  width: 120px;
-  height: 120px;
-  animation: spin 2s linear infinite; */
-
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-`;
-
 function SearchLoading() {
   return (
     <SearchLoadingContainer>
       <h1>Chargement ...</h1>
-      <Spinner />
+      <Spinner animation="border" />
     </SearchLoadingContainer>
   );
 }
