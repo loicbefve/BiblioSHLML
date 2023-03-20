9a38190d9e90cbc56a4082d5d142510bf2ce8355
loicbefve <befve.loic@gmail.com>
2023-03-20 21:52:35 +0100

    Remplissage de la Page Index Pays Lorrain

--- src/pages/IndexPaysLorrain.tsx
+++ src/pages/IndexPaysLorrain.tsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+import CompleteSearchComponent from '../components/search_components/CompleteSearchComponent';
+import { mockImprimes } from '../utils/MockData';
+
+/* STYLED COMPONENTS */
+const Title = styled.h3`
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const ImprimesWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const searchInvitationMessage =
+  "Effectuez votre recherche dans l'Index du Pays Lorrain de la société en utilisant le" +
+  ' formulaire ci-dessus';
+
+/* COMPONENT */
 function IndexPaysLorrain() {
-  return <div>This is the IndexPaysLorrain.tsx component</div>;
+  return (
+    <ImprimesWrapper>
+      <Title>Recherche dans la liste des Imprimés</Title>
+      <hr />
+      <CompleteSearchComponent
+        searchInvitationMessage={searchInvitationMessage}
+        apiURLToCall={mockImprimes.data}
+      />
+    </ImprimesWrapper>
+  );
 }
 
 export default IndexPaysLorrain;
