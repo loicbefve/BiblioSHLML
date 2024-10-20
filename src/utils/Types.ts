3042cbbcd68ff5fa38bd7c380602eebff4f48205
lbefve <loic.befve@gensdeconfiance.com>
2024-10-20 23:42:35 +0200

    feat: refactor last search to use api service

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,10 +1,8 @@
-// FIXME: We use directly types from the generated api client. It could be better
-// to create our own types for less coupling. However the api should not change
-// in a while.
-
-export enum PageState {
+enum PageState {
   NoData = 'nodata',
   Loading = 'loading',
   Loaded = 'loaded',
   Error = 'error',
 }
+
+export default PageState;
